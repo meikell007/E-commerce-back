b6bd888894c285ee6fd39f6266a283f5441ee700
ErronBlood <94480745+ErronBlood@users.noreply.github.com>
2026-04-09 20:30:06 -0400

    Add files via upload

---
+++ src/main/java/com/unimag/ecommercexyz/EcommercexyzApplication.java
@@ -0,0 +1,13 @@
+package com.unimag.ecommercexyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercexyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercexyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unimag/ecommercexyz/EcommercexyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimag.ecommercexyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercexyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
