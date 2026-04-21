79207be512e0b936e189a253167e60b2ab215948
Saxyz <sarmientun@gmail.com>
2026-04-21 00:17:12 -0500

    feat: cache is working in findOrderWithItems endpoint

--- src/main/java/com/unimag/ecommercexyz/config/RedisConfig.java
+++ src/main/java/com/unimag/ecommercexyz/config/RedisConfig.java
@@ -1,18 +1,18 @@
 package com.unimag.ecommercexyz.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.*;
 
 import java.time.Duration;
 
-@EnableRedisRepositories
+@EnableCaching
 @Configuration
 public class RedisConfig {
 
@@ -37,7 +37,6 @@ public class RedisConfig {
         GenericJackson2JsonRedisSerializer genericSerializer = createGenericJacksonRedisSerializer();
 
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .prefixCacheNameWith("orders")
                 .disableCachingNullValues()
                 .entryTtl(Duration.ofMinutes(10))
                 .serializeKeysWith(
