da9e6d0adbf535d5cd8b4bb3863853953c99feb5
Saxyz <sarmientun@gmail.com>
2026-04-20 23:29:46 -0500

    feat: logs added

--- src/main/java/com/unimag/ecommercexyz/service/UserDashboardServiceImpl.java
+++ src/main/java/com/unimag/ecommercexyz/service/UserDashboardServiceImpl.java
@@ -10,6 +10,8 @@ import com.unimag.ecommercexyz.mapper.UserDashboardMapper;
 import com.unimag.ecommercexyz.properties.ServiceProperties;
 import com.unimag.ecommercexyz.repository.UserDashboardRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -22,9 +24,11 @@ public class UserDashboardServiceImpl implements UserDashboardService {
     private final UserDashboardRepository repository;
     private final ServiceProperties properties;
     private final UserDashboardMapper userDashboardMapper;
+    private static final Logger log = LoggerFactory.getLogger(UserDashboardServiceImpl.class);
 
     @Override
     public ProfileDTO findProfileAndOrderHistory(String userId) {
+        log.info("CACHE MISS - searching profile and order history in DynamoDB {}", userId);
         UserEntity user = repository.findUserProfile(userId).orElseThrow(() -> new UserNotFoundException("User not found."));
         List<OrderEntity> orders = repository.findLastOrders(userId, properties.historyOrderLimit());
 
@@ -34,6 +38,7 @@ public class UserDashboardServiceImpl implements UserDashboardService {
     @Cacheable(value = "orders", key = "#orderId")
     @Override
     public OrderDetailsDTO findOrderWithItems(String orderId) {
+        log.info("CACHE MISS - searching order in DynamoDB {}", orderId);
         OrderItemsAggregate orderWithItems = repository.findOrderDetails(orderId);
 
         return userDashboardMapper.orderAndItemsToOrderDetailsDTO(orderWithItems);
